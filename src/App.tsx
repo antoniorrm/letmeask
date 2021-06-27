3c9edca7f04bd26d80bcf0c1a65d6dddb01bf70c
Antonio Raimundo <antoniorrm@sysautomacao.com.br>
2021-06-27 09:40:53 -0300

    feat: second class finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
+
+export function NewRoom() {
+    const { user } = useAuth();
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando pergutnas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiencia em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input type="text" placeholder="Nome da sala"/>
+                        <Button type="submit">
+                            Criar sala
+                        </Button>
+                    </form>
+                    <p>Quer entrar em uma já sala existente? <Link to="/">clique aqui</Link></p>
+                </div>
+            </main>
+        </div>
+    )
+}
